ecd8f59bda15a607cb0d9700ca50334abf534c5e
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-09 21:21:27 -0300

    Criação da função que exibe os livros do autor que o usuário inseriu a partir de um determinado ano.

--- biblioteca.js
+++ biblioteca.js
@@ -6,9 +6,9 @@ let idioma = ["Latim", "Hebraico", "Português", "Latim", "JP"]
 let livroAux = []
 let contador = parseInt(-1)
 
-console.log(ExibirLivrosAno(12))
+console.log(ExibirLivrosAutorAno("Anderson", 2008))
 
-function ExibirLivrosAutor(autorParametro) {
+function ExibirLivrosAutorA(autorParametro) {
     for (let index = 0; index < autorArray.length; index++) {
         if (autorParametro == autorArray[index]) {
             contador++
@@ -22,6 +22,17 @@ function ExibirLivrosAutor(autorParametro) {
 function ExibirLivrosAno(anoParametro){
     for (let index = 0; index < anoArray.length; index++) {
         if (anoParametro == anoArray[index]) {
+            contador++
+            livroAux[contador] = tituloArray[index]
+        }
+    }
+    contador = -1
+    return livroAux
+}
+
+function ExibirLivrosAutorAno(autorParametro,anoParametro){
+    for (let index = 0; index < autorArray.length; index++) {
+        if (autorParametro == autorArray[index] && anoParametro <= anoArray[index]) {
             contador++
             livroAux[contador] = tituloArray[index]
         }
